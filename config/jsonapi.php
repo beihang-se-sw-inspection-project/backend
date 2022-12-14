4499bb5f1121194a8b9ef5d4583434e858eb52d2
simon <slemonla13@gmail.com>
2022-12-14 22:17:21 +0800

    [simon]chore: json api rules

--- config/jsonapi.php
+++ config/jsonapi.php
@@ -1,9 +1,8 @@
 <?php
 
-
 return [
     'resources' => [
-        'auth' => [                        
+        'auth' => [
             'validationRules' => [
                 'create' => [
                     'data.attributes.name' => 'required_without:data.attributes.email|string',
@@ -12,52 +11,126 @@ return [
                 ],
             ],
         ],
-        'passwordresetcode' => [                        
+        'passwordresetcode' => [
             'validationRules' => [
                 'create' => [
                     'data.attributes.email' => 'required|string|email|exists:users,email',
                 ],
                 'update' => [
-                ]
+                ],
             ],
         ],
-        'posts' => [
+        'projects' => [
             'allowedSorts' => [
-                'title',
-                'created_at',
-                'updated_at',
-                'id',
+                'project_name',
+                'project_manager',
+                'assignee',
+                'date',
+                'priority',
+                'overview',
+                'to_do',
             ],
             'allowedIncludes' => [
-                'users',
-                'comments',
-                'postlikes',
-                'hasCurrentLiked'
+
             ],
             'allowedFilters' => [
-                'user_id',
-                'description',
-                'title'
+                'project_name',
+                'project_manager',
+                'assignee',
+                'date',
+                'priority',
+                'overview',
+                'to_do',
             ],
             'validationRules' => [
                 'create' => [
-                    'data.attributes.title' => 'required|string',
-                    'data.attributes.description' => 'required|string',
+
+                    'data.attributes.project_name' => 'required|string',
+                    'data.attributes.project_manager' => 'required|string',
+                    'data.attributes.assignee' => 'required|string',
+                    'data.attributes.date' => 'required|string',
+                    'data.attributes.priority' => 'required|string',
+                    'data.attributes.overview' => 'required|string',
+                    'data.attributes.to_do' => 'required|string',
                 ],
                 'update' => [
-                    'data.attributes.title' => 'sometimes|required|string',
-                    'data.attributes.description' => 'sometimes|required|string',                    
+                    'data.attributes.project_name' => 'sometimes|required|string',
+                    'data.attributes.project_manager' => 'sometimes|required|string',
+                    'data.attributes.assignee' => 'sometimes|required|string',
+                    'data.attributes.date' => 'sometimes|required|string',
+                    'data.attributes.priority' => 'sometimes|required|string',
+                    'data.attributes.overview' => 'sometimes|required|string',
+                    'data.attributes.to_do' => 'sometimes|required|string',
                 ],
             ],
             'relationships' => [
-                [
-                    'type' => 'users',
-                    'method' => 'users',
+
+            ],
+        ],
+        'tasks' => [
+            'allowedSorts' => [
+                'project_id',
+                'status',
+                'deadline',
+                'priority',
+                'task_detail',
+            ],
+            'allowedIncludes' => [
+
+            ],
+            'allowedFilters' => [
+                'project_id',
+                'status',
+                'deadline',
+                'priority',
+                'task_detail',
+            ],
+            'validationRules' => [
+                'create' => [
+
+                    'data.attributes.project_id' => 'required|string',
+                    'data.attributes.status' => 'required|string',
+                    'data.attributes.deadline' => 'required|string',
+                    'data.attributes.priority' => 'required|string',
+                    'data.attributes.task_detail' => 'required|string',
                 ],
-                [
-                    'type' => 'comments',
-                    'method' => 'comments',
+                'update' => [
+                    'data.attributes.project_id' => 'sometimes|required|string',
+                    'data.attributes.status' => 'sometimes|required|string',
+                    'data.attributes.deadline' => 'sometimes|required|string',
+                    'data.attributes.priority' => 'sometimes|required|string',
+                    'data.attributes.task_detail' => 'sometimes|required|string',
                 ],
+            ],
+            'relationships' => [
+
+            ],
+        ],
+        'inspections' => [
+            'allowedSorts' => [
+                'report',
+                'status',
+            ],
+            'allowedIncludes' => [
+
+            ],
+            'allowedFilters' => [
+                'report',
+                'status',
+            ],
+            'validationRules' => [
+                'create' => [
+
+                    'data.attributes.report' => 'required|string',
+                    'data.attributes.status' => 'required|string',
+                ],
+                'update' => [
+                    'data.attributes.report' => 'sometimes|required|string',
+                    'data.attributes.status' => 'sometimes|required|string',
+                ],
+            ],
+            'relationships' => [
+
             ],
         ],
         'users' => [
@@ -77,89 +150,19 @@ return [
                     'data.attributes.name' => 'required|string|max:32|regex:/^[a-zA-Z0-9 ]+$/|unique:users,name',
                     'data.attributes.email' => 'required|string|email|max:255|unique:users,email',
                     'data.attributes.password' => 'required|string|confirmed|min:6|max:16',
-                    'avatar'  => 'sometimes|required|string',
+                    'avatar' => 'sometimes|required|string',
                 ],
                 'update' => [
                     'data.attributes.name' => 'sometimes|required|string|max:32|regex:/^[a-zA-Z0-9 ]+$/|unique:users,name',
                     'data.attributes.email' => 'sometimes|required|email||max:255|unique:users,email',
                     'data.attributes.password' => 'sometimes|required|string|min:6|max:16',
-                    'avatar'  => 'sometimes|required|string',
+                    'avatar' => 'sometimes|required|string',
                 ],
             ],
             'relationships' => [
-                [
-                    'type' => 'comments',
-                    'method' => 'comments',
-                ],
-                [
-                    'type' => 'posts',
-                    'method' => 'posts',
-                ],
-            ],
-        ],
-        'comments' => [
-            'allowedSorts' => [
-                'created_at',
-                'id'
 
             ],
-            'allowedIncludes' => [
-                'posts',
-                'users',
-                'posts.comments',
-                'posts.postlikes'
-            ],
-            'allowedFilters' => [
-                Spatie\QueryBuilder\AllowedFilter::exact('post_id'),
-            ],
-            'validationRules' => [
-                'create' => [
-                    'data.attributes.message' => 'required|string',
-                ],
-                'update' => [
-                    'data.attributes.message' => 'sometimes|required|string',
-                ],
-            ],
-            'relationships' => [
-                [
-                    'type' => 'posts',
-                    'method' => 'posts',
-                ],
-                [
-                    'type' => 'users',
-                    'method' => 'users',
-                ],
-            ],
         ],
-        'postlikes' => [
-            'allowedSorts' => [
-                'created_at',
-            ],
-            'allowedIncludes' => [
-                'posts',
-                'users',
-            ],
-            'allowedFilters' => [
-                Spatie\QueryBuilder\AllowedFilter::exact('post_id'),
-                Spatie\QueryBuilder\AllowedFilter::exact('user_id'),
-            ],
-            'validationRules' => [
-                'create' => [
-                    'data.attributes.post_id' => 'required|string',
-                ],
-                'update' => [
-                    'data.attributes.user_id' => 'required|string',
-                ],
-            ],
-            'relationships' => [],
-        ],
-        'files' => [
-            'validationRules' => [
-                'create' => [
-                    'file'  => 'required|mimes:png,jpg,jpeg,gif|max:2048',
-                ],
-            ],
-            'relationships' => [],
-        ]
+
     ],
 ];
